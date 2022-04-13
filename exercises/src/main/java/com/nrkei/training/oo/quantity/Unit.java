6db951fb41543a48924da6c393b1f12cedab0f23
Fred George <fredgeorge@acm.org>
2022-04-13 15:55:49 +0200

    Fred: Volume Units defined with relative sizes; not exploited yet

--- exercises/src/main/java/com/nrkei/training/oo/quantity/Unit.java
+++ exercises/src/main/java/com/nrkei/training/oo/quantity/Unit.java
@@ -6,14 +6,26 @@
 
 package com.nrkei.training.oo.quantity;
 
+import com.nrkei.training.oo.probability.Chance;
+
+import java.awt.image.PixelInterleavedSampleModel;
+
 public class Unit {
     public static final Unit TEASPOON = new Unit();
-    public static final Unit TABLESPOON = new Unit();
-    public static final Unit OUNCE = new Unit();
-    public static final Unit CUP = new Unit();
-    public static final Unit PINT = new Unit();
-    public static final Unit QUART = new Unit();
-    public static final Unit GALLON = new Unit();
+    public static final Unit TABLESPOON = new Unit(3, TEASPOON);
+    public static final Unit OUNCE = new Unit(2, TABLESPOON);
+    public static final Unit CUP = new Unit(8, OUNCE);
+    public static final Unit PINT = new Unit(2, CUP);
+    public static final Unit QUART = new Unit(2, PINT);
+    public static final Unit GALLON = new Unit(4, QUART);
 
-    private Unit() {}
+    private final double baseUnitRatio;
+
+    private Unit() {
+        baseUnitRatio = 1.0;
+    }
+
+    private Unit(double relativeRatio, Unit relativeUnit) {
+        baseUnitRatio = relativeRatio * relativeUnit.baseUnitRatio;
+    }
 }
