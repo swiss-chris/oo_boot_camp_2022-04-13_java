78156c598fa3cd8acc93309b69fb71ea58a8f22b
ue70941 <christopher.dickinson@sbb.ch>
2022-04-16 02:11:06 +0200

    implement add()

--- exercises/src/main/java/com/nrkei/training/oo/quantity/Unit.java
+++ exercises/src/main/java/com/nrkei/training/oo/quantity/Unit.java
@@ -60,8 +60,12 @@ public abstract class Unit {
     }
 
     double convertedAmount(double otherAmount, Unit other) {
+        return baseUnitAmount(otherAmount, other) / this.baseUnitRatio + this.offset;
+    }
+
+    double baseUnitAmount(double otherAmount, Unit other) {
         if (!this.isCompatible(other)) throw new IllegalArgumentException("Incompatible Units");
-        return (otherAmount - other.offset) * other.baseUnitRatio / this.baseUnitRatio + this.offset;
+        return (otherAmount - other.offset) * other.baseUnitRatio;
     }
 
     int hashCode(double amount) {
