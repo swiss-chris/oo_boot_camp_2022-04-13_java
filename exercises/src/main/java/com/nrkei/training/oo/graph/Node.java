1e7adf0e25b6a29418bb401f9dc183d42f29775d
Fred George <fredgeorge@acm.org>
2022-04-19 08:28:09 +0200

    Fred: Graph with hopCount(); not necessarily minimum; completely refactored

--- exercises/src/main/java/com/nrkei/training/oo/graph/Node.java
+++ exercises/src/main/java/com/nrkei/training/oo/graph/Node.java
@@ -21,7 +21,7 @@ public class Node {
     }
 
     public boolean canReach(Node destination) {
-        return canReach(destination, noVisitedNodes());
+        return hopCount(destination, noVisitedNodes()) != UNREACHABLE;
     }
 
     public int hopCount(Node destination) {
@@ -39,16 +39,6 @@ public class Node {
             if (neighborHopCount != UNREACHABLE) return neighborHopCount + 1;
         }
         return UNREACHABLE;
-    }
-
-    private boolean canReach(Node destination, List<Node> visitedNodes) {
-        if (this == destination) return true;
-        if (visitedNodes.contains(this)) return false;
-        visitedNodes.add(this);
-        for(Node n : neighbors) {
-            if (n.canReach(destination, visitedNodes)) return true;
-        }
-        return false;
     }
 
     private List<Node> noVisitedNodes() {
